61f26dc5dc7a207fea62fbea5343348e816289f4
David Michaels <david_michaels@hms.harvard.edu>
2024-08-31 13:03:50 -0400

    etc

--- src/hms_utils/aws_env.sh
+++ src/hms_utils/aws_env.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 DIR=`realpath $(dirname $BASH_SOURCE)`
 
-# Wrapper script to aws_env.py (which manages SSO/Okta-based AWS credentials in ~/.aws/config),
-# to allow the --current option to actually work WRT setting the AWS_PROFILE environment
-# variable from the calling (command-line/shell) process; since simply running a (Python)
-# cannot set environment variables from the calling (command-line-shell) process.
-# To use this you need to put an alias like this is your ~/.bash_profile or whatever:
-# alias awsenv='source ~/repos/c4-scripts/dmichaels/scripts/aws_env.sh'
-# The fact that it is an alias, and the 'source' there, is crucial to allow
+# Wrapper script for the poetry script hms-aws-env (which is implement in aws_env.py),
+# which manages SSO/Okta-based AWS credentials in ~/.aws/config), to allow the --current
+# option to actually work WRT setting the AWS_PROFILE environment # variable from the
+# calling (command-line/shell) process; since simply running a (Python) cannot set
+# environment variables from the calling (command-line-shell) process. To use this
+# you need to put an alias like this is your ~/.bash_profile or wherever:
+#
+# >>> alias awsenv='source hms-aws-env --shell'
+#
+# The fact that this is an alias, and the 'source' there, is crucial to allow
 # your AWS_PROFILE environment variable to be set (or unset for nocurrent).
 # Then use according to the usage notes in aws_env.py, e.g.:
 #
@@ -15,9 +18,8 @@ DIR=`realpath $(dirname $BASH_SOURCE)`
 #        awsenv default [profile-name] | awsenv nodefault
 #        awsenv current [profile-name] | awsenv nocurrent
 #        awsenv refresh [profile-name]
-
-SCRIPTDIR=~/repos/hms-utils/src/hms_utils
-SCRIPT="python $SCRIPTDIR/aws_env.py"
+#
+SCRIPT=hms-aws-env
 TMPFILE=/tmp/.aws_env-$RANDOM$RANDOM-`date +%Y%m%d%H%M%S`
 $SCRIPT --current-export-file $TMPFILE $*
 STATUS=$?
