ca6059df1686ffb954aa041328680f30a7e5d603
David Michaels <david_michaels@hms.harvard.edu>
2024-09-01 08:11:37 -0400

    tec

--- tests/test_hmsconfig.py
+++ tests/test_hmsconfig.py
@@ -122,3 +122,22 @@ def test_hmsconfig_c():
     }
     config = Config(config)
     assert config.lookup("A/B/C/C3") == "b3value_123_456_b2value_123"
+
+
+def test_hmsconfig_d():
+    config = {
+        "foursight": {
+            "SSH_TUNNEL_ELASTICSEARCH_NAME_PREFIX": "ssh_tunnel_elasticsearch_proxy",
+            "SSH_TUNNEL_ELASTICSEARCH_NAME": "${SSH_TUNNEL_ELASTICSEARCH_NAME_PREFIX}_${SSH_TUNNEL_ELASTICSEARCH_ENV}_${SSH_TUNNEL_ELASTICSEARCH_PORT}",  # noqa
+            "ES_HOST_LOCAL": "http://localhost:${SSH_TUNNEL_ELASTICSEARCH_PORT}",
+            "smaht": {
+                "wolf": {
+                    "SSH_TUNNEL_ELASTICSEARCH_PORT": 9209,
+                    "SSH_TUNNEL_ELASTICSEARCH_ENV": "smaht_wolf",
+                    "SSH_TUNNEL_ELASTICSEARCH_NAME": "${foursight/SSH_TUNNEL_ELASTICSEARCH_NAME}"
+                }
+            }
+        }
+    }
+    config = Config(config)
+    assert config.lookup("foursight/smaht/wolf/ES_HOST_LOCAL") == "http://localhost:9209"
