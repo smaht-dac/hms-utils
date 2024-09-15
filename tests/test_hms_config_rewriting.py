31ecb2b2363204a99d1acb88c0a4addf1f41e926
David Michaels <david_michaels@hms.harvard.edu>
2024-09-15 13:43:31 -0400

    etc

--- tests/test_hms_config_rewriting.py
+++ tests/test_hms_config_rewriting.py
@@ -1,13 +1,11 @@
 import os
 from hms_utils.hms_config_rewriting import Config
-from unittest.mock import patch
 
 TESTS_DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
 
 
 def test_hmsconfig_g():
 
-    config_file = os.path.join(TESTS_DATA_DIR, "config.json")
     config = Config({
         "alfa": "alfa_value",
         "bravo": {
