caf4d242e54167ea1fb4794087cb8b91c6960103
David Michaels <david_michaels@hms.harvard.edu>
2024-09-15 11:43:39 -0400

    etc

--- src/hms_utils/hms_config_rewriting.py
+++ src/hms_utils/hms_config_rewriting.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import json
 import io
 import os
@@ -7,18 +8,13 @@ from typing import Any, List, Optional, Tuple, Union
 from hms_utils.dictionary_utils import JSON
 from hms_utils.misc_utils import is_primitive_type
 
-DEFAULT_PATH_SEPARATOR = "/"
-# DEFAULT_CONFIG_FILE = os.path.expanduser("~/.config/hms/config.json")
-# DEFAULT_SECRETS_FILE = os.path.expanduser("~/.config/hms/secrets.json")
-DEFAULT_CONFIG_FILE = os.path.expanduser("~/repos/hms-utils/config.json")
-DEFAULT_SECRETS_FILE = os.path.expanduser("~/repos/hms-utils/secrets.json")
-
 
 class Config:
 
+    _PATH_SEPARATOR = "/"
     _PATH_COMPONENT_PARENT = ".."
     _PATH_COMPONENT_CURRENT = "."
-    _PATH_COMPONENT_ROOT = DEFAULT_PATH_SEPARATOR
+    _PATH_COMPONENT_ROOT = _PATH_SEPARATOR
     _MACRO_START = "${"
     _MACRO_END = "}"
     _MACRO_PATTERN = re.compile(r"\$\{([^}]+)\}")
@@ -33,7 +29,11 @@ class Config:
     _IMPORTED_CONFIG_KEY_PREFIX = "@@@__CONFIG__:"
     _IMPORTED_SECRETS_KEY_PREFIX = "@@@__SECRETS__:"
 
-    def __init__(self, config: JSON, path_separator: str = DEFAULT_PATH_SEPARATOR) -> None:
+    def __init__(self, config: JSON, path_separator: Optional[str] = None) -> None:
+        if not (isinstance(path_separator, str) and (path_separator := path_separator.strip())):
+            path_separator = Config._PATH_SEPARATOR
+        if not isinstance(config, JSON):
+            config = JSON(config) if isinstance(config, dict) else {}
         self._json = config
         self._path_separator = path_separator
         self._ignore_missing_macro = True
@@ -65,44 +65,34 @@ class Config:
     def lookup(self, path: str, config: Optional[JSON] = None) -> Optional[Union[Any, JSON]]:
         value, context = self._lookup(path, config)
         return value
-        if config is None:
-            config = self._json
-        value = config
-        for path_component in self.unpack_path(path):
-            if path_component == Config._PATH_COMPONENT_PARENT:
-                value = value.parent
-            elif (value := value.get(path_component)) is None:
-                return None
-        return value
 
-    def _lookup(self, path: str, config: Optional[JSON] = None) -> Tuple[Optional[Union[Any, JSON]], JSON]:
-        if config is None:
+    def _lookup(self, path: str, config: Optional[JSON] = None,
+                parents: bool = False) -> Tuple[Optional[Union[Any, JSON]], JSON]:
+        if (config is None) or (not isinstance(config, JSON)):
             config = self._json
-        value = None
-        context = None
-        context_current_only = False  # noqa
-        if path_components := self.unpack_path(path):
-            if path_components[0] == Config._PATH_COMPONENT_ROOT:
-                config = config.root
-                path_components = path_components[1:]
-            elif path_components[0] == Config._PATH_COMPONENT_CURRENT:
-                context_current_only = True  # noqa
-                path_components = path_components[1:]
-            path_components_last_index = len(path_components) - 1
-            for path_component_index, path_component in enumerate(path_components):
-                if path_component == Config._PATH_COMPONENT_PARENT:
-                    if (config := config.parent) is None:
-                        return None, None
-                elif (value := config.get(path_component)) is not None:
-                    if isinstance(value, JSON):
-                        context = config = value
-                    elif path_component_index == path_components_last_index:
-                        return value, context
-                    else:
-                        # Found terminal (non-dictionary) value but not last in path so not found.
-                        return None, context
-                else:
-                    return None, context
+        if (not (path_components := self.unpack_path(path))) or (path_components == [Config._PATH_COMPONENT_ROOT]):
+            # No valid path or just the trival root ("/") path.
+            return None, config
+        if path_root := (path_components[0] == Config._PATH_COMPONENT_ROOT):
+            config = config.root
+            path_components = path_components[1:]
+        value = context = None
+        path_component_index_last = len(path_components) - 1
+        for path_component_index, path_component in enumerate(path_components):
+            context = config
+            if (value := config.get(path_component)) is None:
+                break
+            if isinstance(value, JSON):
+                # Found a JSON in the path so recurse down to it.
+                config = value
+            elif path_component_index < path_component_index_last:
+                # Found a terminal (non-JSON) in the path but it is not the last component.
+                value = None
+                break
+        if (value is None) and (path_component_index > 0) and (parents is not False):
+            # Search for the remaining path up through parents simulating inheritance.
+            path_components = path_components[0:path_component_index - 1] + path_components[path_component_index:]
+            return self._lookup(self.repack_path(path_components, path_root), context.parent)
         return value, context
 
     def lookup_macro(self, macro_value: str, config: Optional[JSON] = None) -> Optional[Union[Any, JSON]]:
@@ -116,26 +106,55 @@ class Config:
         return None
 
     def unpack_path(self, path: str) -> List[str]:
+        return Config._unpack_path(path, path_separator=self._path_separator)
+
+    def repack_path(self, path_components: List[str], root: bool = False) -> str:
+        return Config._repack_path(path_components, root=root, path_separator=self._path_separator)
+
+    def normalize_path(self, path: str) -> List[str]:
+        return self.repack_path(self.unpack_path(path))
+
+    @staticmethod
+    def _unpack_path(path: str, path_separator: Optional[str] = None) -> List[str]:
+        if not (isinstance(path_separator, str) and (path_separator := path_separator.strip())):
+            path_separator = Config._PATH_SEPARATOR
         path_components = []
         if isinstance(path, str) and (path := path.strip()):
             if path.startswith(Config._PATH_COMPONENT_ROOT):
                 path = path[len(Config._PATH_COMPONENT_ROOT):]
                 path_components.append(Config._PATH_COMPONENT_ROOT)
-            for path_component in path.split(self._path_separator):
-                if (path_component := path_component.strip()):
+            for path_component in path.split(path_separator):
+                if ((path_component := path_component.strip()) and (path_component != Config._PATH_COMPONENT_CURRENT)):
+                    if path_component == Config._PATH_COMPONENT_PARENT:
+                        if len(path_components) > 1:
+                            path_components = path_components[:-1]
+                        continue
                     path_components.append(path_component)
         return path_components
 
-    def repack_path(self, path_components: List[str]) -> str:
-        if isinstance(path_components, list) and path_components:
-            if path_components[0] == self._path_separator:
-                return self._path_separator + self._path_separator.join(path_components[1:])
-            else:
-                return self._path_separator.join(path_components)
-        return ""
+    @staticmethod
+    def _repack_path(path_components: List[str], root: bool = False, path_separator: Optional[str] = None) -> str:
+        if not (isinstance(path_separator, str) and (path_separator := path_separator.strip())):
+            path_separator = Config._PATH_SEPARATOR
+        if not (isinstance(path_components, list) and path_components):
+            path_components = []
+        if path_components[0] == Config._PATH_COMPONENT_ROOT:
+            root = True
+            path_components = path_components[1:]
+        return (path_separator if root else "") + path_separator.join(path_components)
 
-    def normalize_path(self, path: str) -> List[str]:
-        return self.repack_path(self.unpack_path(path))
+    @staticmethod
+    def _normalize_path(path: str, path_separator: Optional[str] = None) -> List[str]:
+        return Config._repack_path(Config._unpack_path(path, path_separator), path_separator)
+
+
+print(Config._normalize_path("//abc//def/ghi"))
+
+DEFAULT_PATH_SEPARATOR = Config._PATH_SEPARATOR
+# DEFAULT_CONFIG_FILE = os.path.expanduser("~/.config/hms/config.json")
+# DEFAULT_SECRETS_FILE = os.path.expanduser("~/.config/hms/secrets.json")
+DEFAULT_CONFIG_FILE = os.path.expanduser("~/repos/hms-utils/tests/data/config.json")
+DEFAULT_SECRETS_FILE = os.path.expanduser("~/repos/hms-utils/tests/data/secrets.json")
 
 
 with io.open(DEFAULT_CONFIG_FILE, "r") as f:
@@ -151,14 +170,17 @@ def warning(message: str) -> None:
     print(f"WARNING: {message}", file=sys.stderr, flush=True)
 
 
+# path = "//abc/def/ghi/../../../../jkl/mno/.."
+# print(Config._unpack_path(path))
+
 path = "/portal/../portal/smaht//wolf"
-path = "//portal/smaht//wolf"
-path = "/xportal/smaht/wolf/IDENTIT"
+path = "/portal/smaht/wolf/IDENTITY"
+path = "/portal/smaht/xyzzy"
+path = "/portal/smaht/identity/smaht/dev"
 # value = config.lookup(path)
 # print(f"{config.normalize_path(path)}: {value}")
 value, context = config._lookup(path)
 print(f"{config.normalize_path(path)}: {value} | {context}")
-exit(0)
 
 print()
 path = "portal/smaht///wolf"
