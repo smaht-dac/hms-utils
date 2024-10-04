ff394f1c7bdeb0b6ed3d169e3775b202c92e13bf
David Michaels <david_michaels@hms.harvard.edu>
2024-10-03 23:22:35 -0400

    etc

--- src/hms_utils/dictionary_print_utils.py
+++ src/hms_utils/dictionary_print_utils.py
@@ -52,10 +52,10 @@ def print_dictionary_tree(data: dict,
                 output(indent + corner + " " + key)
                 inner_indent = (indent if first else indent + (2 * " ")) + (2 * " ")
                 for element_index, element_value in enumerate(value):
-                    element_output_value = "{_}" if isinstance(element_value, dict) else str(element_value)
+                    element_output_value = "{}" if isinstance(element_value, dict) else str(element_value)
                     output(f"{inner_indent}└── [{element_index}]: {element_output_value}")
                     if isinstance(element_value, dict):
-                        traverse(element_value, indent=inner_indent + (10 * " "))
+                        traverse(element_value, indent=inner_indent + (9 * " "))
             else:
                 if paths:
                     key = key_path
