ed2f83425ebe31a4eb98503e3d13e1abba3b7344
David Michaels <david_michaels@hms.harvard.edu>
2024-10-06 08:16:04 -0400

    etc

--- src/hms_utils/config/config_output.py
+++ src/hms_utils/config/config_output.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import Any, List, Optional
+from typing import Any, Optional
 from hms_utils.chars import chars
 from hms_utils.dictionary_utils import print_dictionary_list, print_dictionary_tree
 from hms_utils.config.config import Config
@@ -9,7 +9,7 @@ from hms_utils.terminal_utils import terminal_color
 class ConfigOutput:
 
     @staticmethod
-    def print_tree(config: Config, show: bool = False, secret_paths: Optional[List[str]] = None) -> None:
+    def print_tree(config: Config, show: bool = False) -> None:
         def value_modifier(path: str, value: Any) -> Optional[str]:  # noqa
             nonlocal config
             return ConfigOutput._display_value(config, value=value, show=show)
@@ -22,7 +22,7 @@ class ConfigOutput:
                               value_modifier=value_modifier, arrow_indicator=tree_arrow_indicator)
 
     @staticmethod
-    def print_list(config: Config, show: bool = False, secret_paths: Optional[List[str]] = None) -> None:
+    def print_list(config: Config, show: bool = False) -> None:
         def value_modifier(path: str, value: Any) -> Optional[str]:  # noqa
             nonlocal config
             return ConfigOutput._display_value(config, value=value, show=show)
