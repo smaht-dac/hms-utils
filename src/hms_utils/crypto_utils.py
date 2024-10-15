cb2e0aa1426a925df765a054c5ca3ebe7a548b3a
David Michaels <david_michaels@hms.harvard.edu>
2024-10-15 18:10:11 -0400

    etc

--- src/hms_utils/crypto_utils.py
+++ src/hms_utils/crypto_utils.py
@@ -1,8 +1,8 @@
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-from cryptography.hazmat.primitives import hashes
-from cryptography.fernet import Fernet
 import base64
+from cryptography.fernet import Fernet
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 import os
 
 
