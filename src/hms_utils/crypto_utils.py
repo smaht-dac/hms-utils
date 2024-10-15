70552bfb52b7f4cbe38e7cd8883b4c44b97e8671
David Michaels <david_michaels@hms.harvard.edu>
2024-10-15 18:17:47 -0400

    etc

--- src/hms_utils/crypto_utils.py
+++ src/hms_utils/crypto_utils.py
@@ -1,4 +1,5 @@
 import base64
+from typing import Optional
 from cryptography.fernet import Fernet
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
@@ -6,20 +7,22 @@ from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 import os
 
 
-def encrypt_file(plaintext_file: str, password: str, encrypted_file: str) -> bool:
+def encrypt_file(plaintext_file: str, password: str, encrypted_file: str, prefix: Optional[str] = None) -> bool:
+    if not (isinstance(prefix, str) and (prefix := prefix.strip())):
+        prefix = b"__HMS_CRYPTO__"
     with open(plaintext_file, "rb") as f:
         data = f.read()
     encrypted_data = Fernet(_derive_key_from_password(password, salt := os.urandom(16))).encrypt(data)
-    encrypted_data = base64.urlsafe_b64encode(salt) + b"\n" + base64.urlsafe_b64encode(encrypted_data)
+    encrypted_data = prefix + b"\n" + base64.urlsafe_b64encode(salt) + b"\n" + base64.urlsafe_b64encode(encrypted_data)
     with open(f"{encrypted_file}", "wb") as f:
         f.write(encrypted_data)
     return True
 
 
-def decrypt_file(encrypted_file: str, password: str, decrypted_file: str) -> bool:
+def decrypt_file(encrypted_file: str, password: str, decrypted_file: str, prefix: Optional[str] = None) -> bool:
     with open(encrypted_file, "rb") as f:
         data = f.read().split(b"\n")
-    salt, encrypted_data = base64.urlsafe_b64decode(data[0]), base64.urlsafe_b64decode(data[1])
+    salt, encrypted_data = base64.urlsafe_b64decode(data[1]), base64.urlsafe_b64decode(data[2])
     decrypted_data = Fernet(_derive_key_from_password(password, salt)).decrypt(encrypted_data)
     with open(decrypted_file, "wb") as f:
         f.write(decrypted_data)
