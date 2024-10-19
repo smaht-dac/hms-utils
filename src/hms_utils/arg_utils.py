2ce4e6c3d616185c3316b59a4443f65b45c7c17a
David Michaels <david_michaels@hms.harvard.edu>
2024-10-19 17:14:19 -0400

    etc

--- src/hms_utils/arg_utils.py
+++ src/hms_utils/arg_utils.py
@@ -1,35 +1,73 @@
 from __future__ import annotations
 import sys
 from typing import Any, List, Optional
+from hms_utils.type_utils import to_integer
 
 
-class Args:
+class Argv:
 
     class Arg(str):
 
-        def __new__(cls, value: str, args: Args) -> None:
+        def __new__(cls, value: str, args: Argv) -> None:
             value = (value.strip() if args._strip else value) if isinstance(value, str) else ""
             value = super().__new__(cls, value)
-            value._args = args
+            value._argv = args
             return value
 
         def anyof(self, *values) -> bool:
             def match(value: Any) -> bool:
                 nonlocal self
                 return (isinstance(value, str) and
-                        ((self == value) or (self._args._fuzzy and value.startswith("--") and (self == value[1:]))))
+                        ((self == value) or (self._argv._fuzzy and value.startswith("--") and (self == value[1:]))))
             for value in values:
-                if isinstance(value, list):
+                if isinstance(value, (list, tuple)):
                     for element in value:
-                        if match(value):
+                        if self.anyof(element):
                             return True
                 elif match(value):
                     return True
             return False
 
+        def set_boolean(self, *values) -> bool:
+            if self.anyof(values):
+                if self._set_property(*values, property_value=True) is not None:
+                    return True
+            return False
+
+        def set_integer(self, *values) -> bool:
+            if self.anyof(values):
+                if (value := to_integer(self._argv.peek)) is not None:
+                    if self._set_property(*values, property_value=value) is not None:
+                        self._argv.next
+                        return True
+            return False
+
+        def _set_property(self, *values, property_value: Any = None) -> Optional[object]:
+            def find_target_object(*values) -> Optional[object]:
+                for value in values:
+                    if (not isinstance(value, str)) and hasattr(value, "__dict__"):
+                        return value
+                return None
+            def find_property_name(*value) -> Optional[str]:  # noqa
+                for value in values:
+                    if isinstance(value, (list, tuple)):
+                        for element in value:
+                            if property_name := find_property_name(element):
+                                return property_name
+                    elif isinstance(value, str):
+                        if value.startswith("--") and (value := value[2:].strip()):
+                            return value
+                        elif self._argv._fuzzy and value.startswith("-") and (value := value[2:].strip()):
+                            return value
+            if (target_object := find_target_object(*values)) is not None:
+                if property_name := find_property_name(*values):
+                    setattr(target_object, property_name, property_value)
+                    return target_object
+            return None
+
         @property
         def option(self):
-            return self.startswith("-") if self._args._fuzzy else self.startswith("--")
+            return self.startswith("-") if self._argv._fuzzy else self.startswith("--")
 
         @property
         def empty(self):
@@ -39,33 +77,41 @@ class Args:
         def null(self):
             return self.empty
 
-    def __init__(self, args: Optional[List[str]] = None, fuzzy: bool = True, strip: bool = True, skip: bool = True):
-        self._args = args if isinstance(args, list) and args else (sys.argv[1:] if skip is not False else sys.argv)
+    def __init__(self, args: Optional[List[str]] = None, fuzzy: bool = True,
+                 strip: bool = True, skip: bool = True, delete: bool = False) -> None:
+        self._argv = args if isinstance(args, list) and args else (sys.argv[1:] if skip is not False else sys.argv)
         self._argi = 0
         self._fuzzy = fuzzy is not False
         self._strip = strip is not False
+        self._delete = delete is True
 
     @property
     def peek(self) -> Optional[str]:
-        return Args.Arg(self._args[self._argi], self) if self._argi < len(self._args) else Args.Arg(None, self)
+        return Argv.Arg(self._argv[self._argi], self) if self._argi < len(self._argv) else Argv.Arg(None, self)
 
     @property
     def next(self) -> Optional[str]:
         if (value := self.peek) is not None:
-            self._argi += 1
+            if self._delete:
+                del self._argv[0]
+            else:
+                self._argi += 1
             return value
         return None
 
-    def __iter__(self) -> Args:
+    def __iter__(self) -> Argv:
         return self
 
     def __next__(self) -> Optional[str]:
-        if self._argi >= len(self._args):
+        if self._argi >= len(self._argv):
             raise StopIteration
-        arg = self._args[self._argi]
-        self._argi += 1
-        return Args.Arg(arg, self)
+        arg = self._argv[self._argi]
+        if self._delete:
+            del self._argv[0]
+        else:
+            self._argi += 1
+        return Argv.Arg(arg, self)
 
     @property
     def value(self):
-        return self._args
+        return self._argv
--- src/hms_utils/type_utils.py
+++ src/hms_utils/type_utils.py
@@ -1,10 +1,28 @@
-from typing import Any
+from typing import Any, Optional
 
 primitive_type = (int, float, str, bool)
 
 
 def is_primitive_type(value: Any) -> bool:
     return isinstance(value, primitive_type)
+
+
+def is_integer(value: str) -> bool:
+    if isinstance(value, str) and (value := value.strip()):
+        try:
+            int(value)
+        except Exception:
+            pass
+    return False
+
+
+def to_integer(value: str) -> Optional[int]:
+    if isinstance(value, str) and (value := value.strip()):
+        try:
+            return int(value)
+        except Exception:
+            pass
+    return None
 
 
 def any_of_bool(*booleans) -> bool:
