3d2fdefb7365454fb15519213bb79a2ffb582dd8
David Michaels <david_michaels@hms.harvard.edu>
2024-10-20 00:38:40 -0400

    etc

--- src/hms_utils/arg_utils.py
+++ src/hms_utils/arg_utils.py
@@ -12,7 +12,7 @@ class Argv:
     _FUZZY_OPTION_PREFIX_LENGTH = len(_FUZZY_OPTION_PREFIX)
     _ESCAPE_VALUE = "--"
 
-    class Arg(str):
+    class _Arg(str):
 
         def __new__(cls, value: str, argv: Argv) -> None:
             value = (value.strip() if argv._strip else value) if isinstance(value, str) else ""
@@ -42,14 +42,14 @@ class Argv:
 
         def set_boolean(self, *values) -> bool:
             if self.anyof(values):
-                if self._set_property(*values, property_value=True) is not None:
+                if self._set_property(*values, property_value=True):
                     return True
             return False
 
         def set_integer(self, *values) -> bool:
             if self.anyof(values):
                 if (value := to_integer(self._argv.peek)) is not None:
-                    if self._set_property(*values, property_value=value) is not None:
+                    if self._set_property(*values, property_value=value):
                         self._argv.next
                         return True
             return False
@@ -57,17 +57,15 @@ class Argv:
         def set_string(self, *values) -> bool:
             if self.anyof(values):
                 if ((value := self._argv.peek) is not None) and (not value.option):
-                    if self._set_property(*values, property_value=value) is not None:
+                    if self._set_property(*values, property_value=value):
                         self._argv.next
                         return True
             return False
 
         def set_string_multiple(self, *values) -> bool:
             if self.anyof(values):
-                if ((target_object := self._find_target_object(*values)) and
-                    (property_name := self._find_property_name(*values))):  # noqa
+                if property_name := self._find_property_name(*values):
                     property_values = []
-                    setattr(target_object, property_name, property_values)
                     setattr(self._argv._values, property_name, property_values)
                     while True:
                         if not ((self._argv.peek is not None) and (not self._argv.peek.option)):
@@ -76,37 +74,36 @@ class Argv:
                     return True
             return False
 
-        def _set_property(self, *values, property_value: Any = None) -> Optional[object]:
-            if (target_object := self._find_target_object(*values)) is not None:
-                if property_name := self._find_property_name(*values):
-                    setattr(target_object, property_name, property_value)
-                    setattr(self._argv._values, property_name, property_value)
-                    return target_object
-            return None
+        def _set_property(self, *values, property_value: Any = None) -> bool:
+            if property_name := self._find_property_name(*values):
+                setattr(self._argv._values, property_name, property_value)
+                return True
+            return False
 
-        def _find_target_object(self, *values) -> Optional[object]:
-            for value in values:
-                if (not isinstance(value, str)) and hasattr(value, "__dict__"):
-                    return value
-            return None
-
-        def _find_property_name(self, *values) -> Optional[str]:  # noqa
+        def _find_property_name(self, *values) -> Optional[str]:
             for value in values:
                 if isinstance(value, (list, tuple)):
                     for element in value:
                         if property_name := self._find_property_name(element):
                             return property_name
                 elif isinstance(value, str) and (not self._argv._escaping):
-                    if value.startswith(Argv._OPTION_PREFIX) and (value := value[Argv._OPTION_PREFIX_LENGTH:].strip()):
+                    if value := self._option_to_name(value):
                         return value
-                    elif (self._argv._fuzzy and
-                          value.startswith(Argv._FUZZY_OPTION_PREFIX) and
-                          (value := value[Argv._FUZZY_OPTION_PREFIX_LENGTH:].strip())):
-                        return value
+            return None
 
         @property
         def option(self):
-            return (not self._argv._escaping) and self.startswith("-") if self._argv._fuzzy else self.startswith("--")
+            return self._option_to_name(self) is not None
+
+        def _option_to_name(self, value: str) -> Optional[str]:
+            if isinstance(value, str) and (value := value.strip()):
+                if value.startswith(Argv._OPTION_PREFIX) and (value := value[Argv._OPTION_PREFIX_LENGTH:].strip()):
+                    return value
+                elif (self._argv._fuzzy and
+                      value.startswith(Argv._FUZZY_OPTION_PREFIX) and
+                      (value := value[Argv._FUZZY_OPTION_PREFIX_LENGTH:].strip())):
+                    return value
+            return None
 
         @property
         def empty(self):
@@ -116,14 +113,14 @@ class Argv:
         def null(self):
             return self.empty
 
-    class Values:
+    class _Values:
         unparsed = []
 
     def __init__(self, argv: Optional[List[str]] = None, fuzzy: bool = True,
                  strip: bool = True, skip: bool = True, escape: bool = True, delete: bool = False) -> None:
         self._argv = argv if isinstance(argv, list) and argv else (sys.argv[1:] if skip is not False else sys.argv)
         self._argi = 0
-        self._values = Argv.Values()
+        self._values = Argv._Values()
         self._fuzzy = fuzzy is not False
         self._strip = strip is not False
         self._escape = escape is not False
@@ -132,7 +129,7 @@ class Argv:
 
     @property
     def peek(self) -> Optional[str]:
-        return Argv.Arg(self._argv[self._argi], self) if self._argi < len(self._argv) else Argv.Arg(None, self)
+        return Argv._Arg(self._argv[self._argi], self) if self._argi < len(self._argv) else Argv._Arg(None, self)
 
     @property
     def next(self) -> Optional[str]:
@@ -158,12 +155,12 @@ class Argv:
         if (arg == Argv._ESCAPE_VALUE) and self._escape and (not self._escaping):
             self._escaping = True
             return self.__next__()
-        return Argv.Arg(arg, self)
+        return Argv._Arg(arg, self)
 
     @property
     def list(self) -> List[str]:
         return self._argv
 
     @property
-    def values(self) -> Values:
+    def values(self) -> Argv._Values:
         return self._values
