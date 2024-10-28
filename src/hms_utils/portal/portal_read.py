6ddfbe49c19d4a60e28bb94b3d449cae8b8344db
David Michaels <david_michaels@hms.harvard.edu>
2024-10-28 13:57:26 -0400

    etc

---
+++ src/hms_utils/portal/portal_read.py
@@ -0,0 +1,11 @@
+# TODO: Rewrite/refactor view_portal_object.py
+
+from hms_utils.argv import ARGV  # noqa
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/hms_utils/portal/portal_write.py
@@ -0,0 +1,11 @@
+# TODO: Rewrite/refactor update_portal_object.py
+
+from hms_utils.argv import ARGV  # noqa
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
