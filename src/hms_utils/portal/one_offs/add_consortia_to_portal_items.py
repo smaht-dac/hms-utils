37127bab8c2436b3be7501e85ab5c0feb24bdafb
David Michaels <david_michaels@hms.harvard.edu>
2024-11-01 11:22:25 -0400

    tec

--- src/hms_utils/portal/one_offs/add_consortia_to_portal_items.py
+++ src/hms_utils/portal/one_offs/add_consortia_to_portal_items.py
@@ -2,38 +2,57 @@
 # This bug has been fixed (C4-1186) and this script (C4-1187) is to correct the existing data.
 # Before this was run a backup was made of the smaht-portal (RDS) database: rds-smaht-production-snapshot-20241031
 
-import sys
-import time
 from dcicutils.ff_utils import search_metadata
 from hms_utils.portal.portal_utils import Portal
 from hms_utils.chars import chars
+from hms_utils.threading_utils import run_concurrently
 
-portal_env = "smaht-data"
 portal_env = "smaht-devtest"
 portal_env = "smaht-wolf"
+portal_env = "smaht-staging"
 portal = Portal(portal_env)
 
 
 query = "/search/?type=Item&consortia.display_title=No+value"
-# query = "/search/?type=Item&consortia.display_title=No+value&uuid=57eda24e-a254-46ae-9b23-484a4d24728a"
 ignored_types = ["AccessKey", "TrackingItem", "Consortium", "SubmissionCenter"]
 results = search_metadata(query, key=portal.key, is_generator=True)
 consortia = ["smaht"]
+nerrors = 0
+total = 0
 
+
+def set_consortia(item: dict) -> None:
+    global consortia, total, nerrors
+    try:
+        item_uuid = item.get("uuid")
+        item_type = portal.get_schema_type(item)
+        print(f"{item_uuid}: {item_type} {chars.rarrow_hollow} SETTING CONSORTIA: {consortia}", flush=True)
+        portal.patch_metadata(item_uuid, {"consortia": consortia})
+        total += 1
+    except Exception as e:
+        nerrors += 1
+        print(e, flush=True)
+
+
+print(f"SETTING CONSORTIA FOR PORTAL ITEMS WITHOUT: {portal.server}")
+concurrency = 50
+functions = []
 for item in results:
     if isinstance(item, dict) and (item_uuid := item.get("uuid")):
         if (item_type := portal.get_schema_type(item)) not in ignored_types:
             if (existing_consortia := item.get("consortia", None)) is None:
-                print(f"{item_uuid}: {item_type} {chars.rarrow_hollow} SETTING CONSORTIA: {consortia}")
-                try:
-                    portal.patch_metadata(item_uuid, {"consortia": consortia})
-                except Exception:
-                    print(f"ERROR: Cannot patch {item_uuid}. Trying reindex of this item.", file=sys.stderr, flush=True)
-                    if not portal.reindex_metadata(item_uuid):
-                        print(f"ERROR: Reindex failed: {item_uuid}", file=sys.stderr, flush=True)
-                    else:
-                        time.sleep(2)
-                        print(f"Trying patch again: {item_uuid}", file=sys.stderr, flush=True)
-                        portal.patch_metadata(item_uuid, {"consortia": consortia})
+                print(f"{item_uuid}: {item_type} {chars.rarrow_hollow} SETTING CONSORTIA: {consortia}", flush=True)
+                if True:
+                    functions.append(lambda item=item: set_consortia(item))
+                    if len(functions) >= concurrency:
+                        run_concurrently(functions, nthreads=concurrency)
+                        functions = []
             else:
                 print(f"{item_uuid}: {item_type} {chars.rarrow_hollow} CONSORTIA ALREADY SET")
+
+
+if functions:
+    run_concurrently(functions, nthreads=concurrency)
+
+print(f"DONE SETTING CONSORTIA FOR PORTAL ITEMS WITHOUT:"
+      f"{portal.server} {chars.rarrow_hollow} {total} (errors: {nerrors})")
--- src/hms_utils/portal/portal_validation_errors.py
+++ src/hms_utils/portal/portal_validation_errors.py
@@ -1,5 +1,5 @@
 from dcicutils.ff_utils import search_metadata
-from dcicutils.portal_utils import Portal
+from hms_utils.portal.portal_utils import Portal
 from hms_utils.argv import ARGV
 from hms_utils.chars import chars
 
@@ -8,35 +8,52 @@ def main():
 
     argv = ARGV({
         ARGV.REQUIRED(str): "--env",
+        ARGV.OPTIONAL(bool): "--sid",
+        ARGV.OPTIONAL(bool): "--sid-reindex",
         ARGV.OPTIONAL([str]): "uuids"
     })
 
     portal = Portal(argv.env)
     status = 0
+    uuids_with_sid_error = []
 
     if argv.uuids:
         for uuid in argv.uuids:
             if item := portal.get_metadata(uuid):
-                if _check_for_validation_errors(item):
+                if _check_for_validation_errors(item, sid=argv.sid):
+                    if argv.sid and argv.sid_reindex:
+                        uuids_with_sid_error.append(item.get("uuid"))
                     status
-        exit(status)
 
-    query = "/search/?type=Item&validation_errors.name%21=No+value"
-    ignored_types = []
-    for item in search_metadata(query, key=portal.key, is_generator=True):
-        if isinstance(item, dict) and (item_uuid := item.get("uuid")):
-            if (item_type := portal.get_schema_type(item)) not in ignored_types:
-                print(f"{item_uuid}: {item_type}")
-                if item := portal.get_metadata(item_uuid):
-                    if _check_for_validation_errors(item):
-                        status = 1
+    else:
+        query = "/search/?type=Item&validation_errors.name%21=No+value"
+        ignored_types = []
+        for item in search_metadata(query, key=portal.key, is_generator=True):
+            if isinstance(item, dict) and (item_uuid := item.get("uuid")):
+                if (item_type := portal.get_schema_type(item)) not in ignored_types:
+                    print(f"{item_uuid}: {item_type}")
+                    if item := portal.get_metadata(item_uuid):
+                        if _check_for_validation_errors(item, sid=argv.sid):
+                            if argv.sid and argv.sid_reindex:
+                                uuids_with_sid_error.append(item.get("uuid"))
+                            status = 1
+
+    if uuids_with_sid_error:
+        for uuid_with_sid_error in uuids_with_sid_error:
+            print(f"Reindexing item with sid error : {uuid_with_sid_error}")
+            portal.reindex_metadata(uuid_with_sid_error)
+
     exit(status)
 
 
-def _check_for_validation_errors(item: dict) -> bool:
+def _check_for_validation_errors(item: dict, sid: bool = False) -> bool:
+    found = False
     if errors := item.get("validation-errors"):
         for error in errors:
-            print(f"{chars.rarrow_hollow} ERROR: {error.get('description')}")
+            if (sid is not True) or ("sid" in str(error)):
+                found = True
+                print(f"{chars.rarrow_hollow} ERROR: {error.get('description')}")
+    return found
 
 
 if __name__ == "__main__":
