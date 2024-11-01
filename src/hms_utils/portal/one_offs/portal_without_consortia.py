e344a37824e4d8458056afd7789c4f50f7c529f6
David Michaels <david_michaels@hms.harvard.edu>
2024-11-01 11:23:24 -0400

    etc

---
+++ src/hms_utils/portal/one_offs/portal_without_consortia.py
@@ -0,0 +1,32 @@
+from dcicutils.ff_utils import search_metadata
+from dcicutils.portal_utils import Portal
+from hms_utils.argv import ARGV
+
+
+def main():
+
+    argv = ARGV({
+        ARGV.REQUIRED(str): "--env"
+    })
+
+    portal = Portal(argv.env)
+    status = 0
+
+    query = "/search/?type=Item&consortia.display_title=No+value"
+    ignored_types = ["AccessKey", "TrackingItem", "Consortium", "SubmissionCenter"]
+    total = 0
+    for item in search_metadata(query, key=portal.key, is_generator=True):
+        item_type = portal.get_schema_type(item)
+        if item_type in ignored_types:
+            continue
+        item_uuid = item.get("uuid")
+        print(f"{item_uuid}: {item_type}")
+        total += 1
+
+    print(f"TOTAL: {total}")
+    exit(status)
+
+
+if __name__ == "__main__":
+    main()
+    pass
