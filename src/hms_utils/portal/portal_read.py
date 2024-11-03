74712442820c862fc1cca578d6a63d4359ab9fb2
David Michaels <david_michaels@hms.harvard.edu>
2024-11-03 18:34:59 -0500

    etc

--- src/hms_utils/portal/portal_read.py
+++ src/hms_utils/portal/portal_read.py
@@ -1,33 +1,39 @@
 # TODO: Rewrite/refactor view_portal_object.py
 
+from functools import lru_cache
 import io
 import json
 import os
 import sys
-from typing import Optional
+from typing import Any, List, Optional, Union
 from dcicutils.captured_output import captured_output
 from hms_utils.argv import ARGV
 from hms_utils.chars import chars
 from hms_utils.portal.portal_utils import Portal
+from hms_utils.threading_utils import run_concurrently
+from hms_utils.type_utils import is_uuid
+
+_UUID_PROPERTY_NAME = "uuid"
 
 
 def main():
 
     argv = ARGV({
-        ARGV.REQUIRED(str): ["arg"],
+        ARGV.REQUIRED(str, "58fd2534-8966-412c-a3a6-c12e20ef569b"): ["arg"],
         ARGV.OPTIONAL(str): ["--app"],
-        ARGV.OPTIONAL(str): ["--env", "--e"],
+        ARGV.OPTIONAL(str, "smaht-data"): ["--env", "--e"],
         ARGV.OPTIONAL(str): ["--ini", "--ini-file"],
         ARGV.OPTIONAL(bool): ["--inserts"],
         ARGV.OPTIONAL(bool): ["--insert-files"],
         ARGV.OPTIONAL(str): ["--output", "--out"],
-        ARGV.OPTIONAL(bool): ["--raw"],
-        ARGV.OPTIONAL(bool): ["--database"],
+        ARGV.OPTIONAL(bool, True): ["--raw"],
+        ARGV.OPTIONAL(bool, True): ["--database"],
         ARGV.OPTIONAL(bool): ["--noformat"],
         ARGV.OPTIONAL(bool): ["--json"],
         ARGV.OPTIONAL(bool): ["--yaml", "--yml"],
         ARGV.OPTIONAL(bool): ["--refs", "--ref"],
-        ARGV.OPTIONAL([str]): ["--ignore-fieldss", "--ignore"],
+        ARGV.OPTIONAL(int, 32): ["--nthreads", "--threads"],
+        ARGV.OPTIONAL([str]): ["--ignore-fields", "--ignore"],
         ARGV.OPTIONAL(bool): ["--show"],
         ARGV.OPTIONAL(bool): ["--verbose"],
         ARGV.OPTIONAL(bool): ["--debug"],
@@ -47,6 +53,19 @@ def main():
     result = portal.get_metadata(argv.arg, raw=argv.raw or argv.inserts)
     object_type = portal.get_schema_type(result)
 
+    ignore_uuids = []
+    while True:
+        if not (referenced_uuids := _get_referenced_uuids(result, ignore_uuids=ignore_uuids)):
+            break
+        referenced_items = _get_portal_metadata_for_uuids(portal, referenced_uuids,
+                                                          raw=argv.raw, database=argv.database, nthreads=argv.nthreads)
+        ignore_uuids.extend([item.get(_UUID_PROPERTY_NAME) for item in referenced_items])
+    print("MAIN:")
+    print(json.dumps(result, indent=4))
+    print("REFS:")
+    print(json.dumps(referenced_items, indent=4))
+    exit(0)
+
     if argv.debug:
         _print(f"OBJECT TYPE: {object_type}")
 
@@ -59,6 +78,54 @@ def main():
         _print(result)
     else:
         _print(json.dumps(result, indent=4))
+
+
+def _get_referenced_uuids(item: dict, ignore_uuids: Optional[List[str]] = None) -> List[str]:
+    referenced_uuids = []
+    def find_referenced_uuids(item: Any) -> List[str]:  # noqa
+        nonlocal referenced_uuids, ignore_uuids
+        if isinstance(item, dict):
+            for value in item.values():
+                find_referenced_uuids(value)
+        elif isinstance(item, (list, tuple)):
+            for element in item:
+                find_referenced_uuids(element)
+        elif is_uuid(item) and (item not in ignore_uuids) and (item not in referenced_uuids):
+            referenced_uuids.append(item)
+    if isinstance(item, dict) and (uuid := item.get(_UUID_PROPERTY_NAME)):
+        if isinstance(ignore_uuids, list):
+            if uuid not in ignore_uuids:
+                ignore_uuids.append(uuid)
+        else:
+            ignore_uuids = [uuid]
+    elif not isinstance(ignore_uuids, list):
+        ignore_uuids = []
+    find_referenced_uuids(item)
+    return referenced_uuids
+
+
+def _get_portal_metadata_for_uuids(portal: Portal, uuids: Union[List[str], str],
+                                   raw: bool = False, database: bool = False,
+                                   nthreads: Optional[int] = None) -> List[str]:
+    items = []
+    if not isinstance(uuids, list):
+        if not (isinstance(uuids, str) and is_uuid(uuids)):
+            return []
+        uuids = [uuids]
+    fetch_metadata_functions = []
+    def fetch_portal_metatadata(uuid: str) -> Optional[dict]:  # noqa
+        nonlocal portal, raw, database, items
+        if item := _get_portal_metatadata_for_uuid(portal, uuid, raw=raw, database=database):
+            items.append(item)
+    if fetch_metadata_functions := [lambda uuid=uuid: fetch_portal_metatadata(uuid) for uuid in uuids if is_uuid(uuid)]:
+        run_concurrently(fetch_metadata_functions, nthreads=nthreads)
+    return items
+
+
+@lru_cache(maxsize=1024)
+def _get_portal_metatadata_for_uuid(portal: Portal, uuid: str,
+                                    raw: bool = False, database: bool = False) -> Optional[dict]:
+    return portal.get_metadata(uuid, raw=raw, database=database, raise_exception=False) if is_uuid(uuid) else None
 
 
 def _create_portal(env: Optional[str] = None, ini: Optional[str] = None, app: Optional[str] = None,
--- src/hms_utils/threading_utils.py
+++ src/hms_utils/threading_utils.py
@@ -6,5 +6,7 @@ def run_concurrently(functions: Iterable[Callable], nthreads: int = 4) -> None:
     # FYI: Not pulling in from dcicutils.misc_utils becausethere is
     # a call to logging.basicConfig() which is (for some reason) causing
     # exceptions within the asynchronous function calls to be output.
+    if not (isinstance(nthreads, int) and (nthreads > 0)):
+        nthreads = 4
     with concurrent.futures.ThreadPoolExecutor(max_workers=nthreads) as executor:
         concurrent.futures.as_completed([executor.submit(f) for f in functions])
