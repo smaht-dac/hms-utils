a478c5238bad88a19055ff1c8c538c04dd7a19b5
David Michaels <david_michaels@hms.harvard.edu>
2024-11-14 16:07:25 -0500

    et

--- src/hms_utils/portal/portal_permissions.py
+++ src/hms_utils/portal/portal_permissions.py
@@ -1,26 +1,57 @@
 import json
 import sys
-from typing import List, Optional, Union
+from typing import List, Literal, Optional, Union
+from hms_utils.argv import ARGV
 from hms_utils.portal.portal_utils import Portal
+
+
+class Action:
+    ADD = "add"
+    EDIT = "edit"
+    CREATE = "create"
+    VIEW = "view"
+    VIEW_DETAILS = "view_details"
+    VISIBLE_FOR_EDIT = "visible_for_edit"
+
+
+ActionType = Literal[Action.ADD, Action.EDIT, Action.CREATE,
+                     Action.VIEW, Action.VIEW_DETAILS, Action.VISIBLE_FOR_EDIT]
 
 
 def main():
 
+    argv = ARGV({
+        ARGV.OPTIONAL(str): ["item"],
+        ARGV.OPTIONAL(str): ["--env"],
+        ARGV.OPTIONAL(str): ["--user"],
+        ARGV.OPTIONAL(bool): ["--ping"],
+        ARGV.OPTIONAL(bool): ["--verbose"],
+        ARGV.OPTIONAL(bool): ["--version"]
+    })
+
     status = 0
 
-    env = "smaht-local"
-    user_env = "smaht-local-feng"
-    target_item_uuid = "670cbe0e-e8b4-4dae-b9ed-8bf33bd91ad5"
+    target_item_uuid = argv.item
 
-    portal = Portal(env)
-    portal_for_user = Portal(user_env)
+    portal = Portal.create(argv.env, verbose=argv.verbose, ping=argv.ping)
+
+    if argv.user:
+        portal_for_user = Portal.create(argv.user or argv.env)
+        if portal.server != portal_for_user.server:
+            print(f"Mismatched Portal environments: {portal.server} vs {portal_for_user.server}")
+            exit(1)
+    else:
+        portal_for_user = portal
 
     principals_for_user = get_principals_for_user(portal_for_user)
     principals_allowed_for_item = get_principals_allowed_for_item(portal, target_item_uuid)
-    view_allowed = is_user_allowed_item_access(principals_for_user, principals_allowed_for_item, "view")
-    edit_allowed = is_user_allowed_item_access(principals_for_user, principals_allowed_for_item, "edit")
+    view_allowed = is_user_allowed_item_access(principals_for_user, principals_allowed_for_item, Action.VIEW)
+    edit_allowed = is_user_allowed_item_access(principals_for_user, principals_allowed_for_item, Action.EDIT)
 
+    print(f"PRINCIPALS FOR USER: {argv.user}")
     print(json.dumps(principals_for_user, indent=4))
+
+    print(f"PRINCIPALS FOR ITEM: {argv.item}")
     print(json.dumps(principals_allowed_for_item, indent=4))
 
     print(view_allowed)
@@ -42,8 +73,9 @@ def get_principals_for_user(portal_or_key_or_key_name: Union[Portal, dict, str])
 
 
 def get_principals_allowed_for_item(portal: Portal, query: str,
-                                    action: Optional[str] = None) -> Optional[Union[dict, List[str]]]:
-    if not (isinstance(action, str) and (action := action.strip())):
+                                    action: Optional[ActionType] = None) -> Optional[Union[dict, List[str]]]:
+    # if not (isinstance(action, str) and (action := action.strip())):
+    if action not in ActionType:
         action = None
     if not (isinstance(portal, Portal) and isinstance(query, str) and query):
         return [] if action else {}
