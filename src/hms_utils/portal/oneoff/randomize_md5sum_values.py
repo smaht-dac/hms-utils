68d13120c96ce66174adce487cdef24fc01a8978
David Michaels <david_michaels@hms.harvard.edu>
2024-11-21 13:33:56 -0500

    adf

--- src/hms_utils/portal/oneoff/randomize_md5sum_values.py
+++ src/hms_utils/portal/oneoff/randomize_md5sum_values.py
@@ -2,9 +2,10 @@ import hashlib
 import io  # noqa
 import json  # noqa
 import os
-from typing import List, Optional, Union
+import sys
+from typing import List, Union
 from uuid import uuid4
-# from dcicutils.command_utils import yes_or_no
+from dcicutils.command_utils import yes_or_no
 
 file = os.path.expanduser("~/repos/etc/repo-extras/portal/4dn/data/exported-from-fourfront-data-experiment-set-replicates-4DNESXP5VE8C-20241120-3-noignore-static-content/file_processed.json")  # noqa
 
@@ -22,26 +23,37 @@ def randomize_md5sum_values(items: Union[List[dict], dict]) -> None:
             randomize_md5sum_values(items[key])
 
 
-def randomize_md5sum_values_in_json_file(file: str, overwrite: bool = False) -> Optional[Union[list, dict]]:
+def randomize_md5sum_values_in_json_file(file: str, overwrite: bool = False, verbose: bool = False) -> bool:
     if not os.path.exists(file):
-        return None
+        if verbose:
+            print(f"File does not exist: {file}", file=sys.stderr, flush=True)
+        return False
     try:
         with io.open(file, "r") as f:
             try:
                 if not (data := json.load(f)):
-                    print(f"Cannot load JSON from file: {file}")
-                    return None
+                    print(f"Cannot load JSON from file: {file}", file=sys.stderr, flush=True)
+                    return False
                 randomize_md5sum_values(data)
-                return data
             except Exception:
-                print(f"Error loading JSON from file: {file}")
-                return None
-        if overwrite is True:
-            pass
+                print(f"Exception loading JSON from file: {file}", file=sys.stderr, flush=True)
+                return False
+        if overwrite is not True:
+            if verbose is not True:
+                return False
+            if not yes_or_no(f"Overwrite file: {file} ?"):
+                return False
+        if verbose is True:
+            print(f"Overwriting file: {file}")
+        try:
+            with io.open(file, "w") as f:
+                json.dump(data, f, indent=4)
+        except Exception:
+            print(f"Exception writing file: {file}")
+            return False
     except Exception:
-        print("Cannot open file: {file}")
-        return None
+        print(f"Cannot open file: {file}")
+        return False
 
 
-data = randomize_md5sum_values_in_json_file(file)
-print(json.dumps(data, indent=4))
+randomize_md5sum_values_in_json_file(file, verbose=True)
